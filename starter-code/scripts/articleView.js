b926ece606f80b67073e0d60d1fa61dc4c5cceb7
timothy mitchell <timothy.p.mitchell92@gmail.com>
2017-05-12 14:51:35 -0700

    the html form is built

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -75,7 +75,7 @@ articleView.setTeasers = function() {
 
 articleView.initNewArticlePage = function() {
   // TODO: Ensure the main .tab-content area is revealed. We might add more tabs later or otherwise edit the tab navigation.
-
+  $('.tab-content').show();
 
   // TODO: The new articles we create will be copy/pasted into our source data file.
   // Set up this "export" functionality. We can hide it for now, and show it once we have data to export.
